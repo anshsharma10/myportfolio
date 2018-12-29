b48c5a7bcaf903e2f5f57f3d44342aa9ad7bf2b3
pivot5 <anshsharma10@yahoo.ca>
2018-12-29 18:24:02 -0500

    Addition of survey form

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
   console.log('request was made: ' + req.url);
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var myReadStream = fs.createReadStream(__dirname + '/doc.html', 'utf8');
-  myReadStream.pipe(res);
+  if(req.url === '/tribute_page' || req.url === '/'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/tributepage.html').pipe(res);
+  } else if(req.url === '/survey'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/survey.html').pipe(res);
+  }
 });
 
 server.listen(3000);
