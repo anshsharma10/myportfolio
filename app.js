b14e3dc3b410758a302668f4796b11ad25f60e4c
pivot5 <anshsharma10@yahoo.ca>
2018-12-30 19:55:58 -0500

    Addition of product landing and main page

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ var server = http.createServer(function(req, res){
   console.log('request was made: ' + req.url);
   if(req.url === '/tribute_page' || req.url === '/'){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/tributepage.html').pipe(res);
+    fs.createReadStream(__dirname + '/main.html').pipe(res);
   } else if(req.url === '/survey'){
     res.writeHead(200, {'Content-Type': 'text/html'});
     fs.createReadStream(__dirname + '/survey.html').pipe(res);
+  } else if(req.url === '/tributepage'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/tributepage.html').pipe(res);
   }
 });
 
